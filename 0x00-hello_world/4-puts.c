2acbbd6dc0852ca1ffd658b6767535fbd76f422f
obasootom <onyearugbulem7@gmail.com>
2023-07-13 14:49:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,11 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */int main(void)
+ */
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
+    char str[] ="\"Programming is like building a multilingual puzzle";
+
+    puts(str);
     return (0);
 }
