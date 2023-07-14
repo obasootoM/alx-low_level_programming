39d378404ce6cca5d7f9660086cead55d5674c2c
obasootom <onyearugbulem7@gmail.com>
2023-07-14 16:45:10 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include  <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, k;
+for (i = 0; i < 98; i++)
+{
+for (k = i + 1; k <= 99; k++)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+putchar((k / 10) + '0');
+putchar((k % 10) + '0');
+if (i == 98 && k == 99)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
