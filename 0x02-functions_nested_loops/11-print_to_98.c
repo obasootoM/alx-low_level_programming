55cad087734274258dae1cfa6e5e4a72ef5410cc
obasootom <onyearugbulem7@gmail.com>
2023-07-18 17:59:14 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_to_98 - checks for lowercase character.
+ * @n: -second value.
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+for (n = 0; n <= 98; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
