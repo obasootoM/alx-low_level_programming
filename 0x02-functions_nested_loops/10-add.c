664a6e97eaf7ec3717f25368078ca76cd682e4ab
obasootom <onyearugbulem7@gmail.com>
2023-07-18 17:49:37 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - checks for lowercase character.
+ * @c: - first value.
+ * @n: -second value.
+ * Return: Always 0.
+ */
+int add(int c, int n)
+{
+return (c + n);
+}
