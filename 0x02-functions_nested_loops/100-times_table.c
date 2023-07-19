ae4ecd72bae93bd4cd2a465782d1dcf6e850607f
obasootom <onyearugbulem7@gmail.com>
2023-07-18 19:20:45 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_times_table - checks for lowercase character.
+ * @n: -second value.
+ * Return: Always 0.
+ */
+void print_times_table(int n)
+{
+int num1, num2, num3;
+if (n >= 0 && n <= 15)
+{
+for (num1 = 0; num1 <= n; num1++)
+{
+_putchar('0');
+for (num2 = 1; num2 <= n; num2++)
+{
+_putchar(',');
+_putchar(' ');
+num3 = num1 * num2;
+if (num3 <= 99)
+_putchar(' ');
+if (num3 <= 9)
+_putchar(' ');
+if (num3 >= 100)
+{
+_putchar((num3 / 100) + '0');
+_putchar((num3 / 10) % 10 + '0');
+}
+else if (num3 <= 99 && num3 >= 10)
+{
+_putchar((num3 / 10) + '0');
+}
+_putchar((num3 % 10) + '0');
+}
+_putchar('\n');
+}
+}
+}
