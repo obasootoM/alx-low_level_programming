9888f684f9a37b13c38574243359854946bf6817
obasootom <onyearugbulem7@gmail.com>
2023-07-20 15:12:45 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - checks for lowercase character.
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int c;
+for (c = 0; c <= 9; c++)
+{
+_putchar(c + '0');
+}
+_putchar('\n');
+}
