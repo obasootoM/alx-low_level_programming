41192c21c7d13f6d980ee5ee34d2ad2ed51eba8b
obasootom <onyearugbulem7@gmail.com>
2023-07-20 15:00:54 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - checks for lowercase character.
+ * @a: the charactert to be checked.
+ * @b: the character to be checked
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
