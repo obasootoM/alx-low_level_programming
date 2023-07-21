bcc9681a9ad78581d4c8fa156fe2ddf6033de7f0
obasootom <onyearugbulem7@gmail.com>
2023-07-20 17:59:29 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * most_numbers - print the numbers since 0 - 9.
+ * Return: return the numbers 0 up to 9.
+ */
+void more_numbers(void)
+{
+int c;
+int i;
+for (c = 0; c < 9; c++)
+{
+for (i = 0; i <= 14; i++)
+{
+if (i > 0)
+}
+_putchar((i / 10) + *);
+{
+_putchar((i % 10) + '0');
+}
+_putchar('\n');
+}
+}
