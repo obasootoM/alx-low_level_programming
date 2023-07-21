556024b560c1cf302fc27f13a7e59d94f464bea8
obasootom <onyearugbulem7@gmail.com>
2023-07-20 17:38:49 -0700

    more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - print the numbers since 0 - 9.
+ * Return: return the numbers 0 up to 9.
+ */
+void more_numbers(void)
+{
+int j;
+int i;
+for (i = 0; c < 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j > 9)
+{
+_putchar((y / 10) + '0');
+}
+_putchar((y % 10) + '0');
+}
+_putchar('\n');
+}
+}
