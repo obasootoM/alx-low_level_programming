017cab362a477d7b38b387bed44882fa53c3b82f
obasootom <onyearugbulem7@gmail.com>
2023-07-24 13:49:05 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - print the numbers since 0 - 9.
+ * @str: number of str
+ * Return: return the numbers 0 up
+ */
+void puts2(char *str)
+{
+int index;
+int len = 0;
+while (str[len] != '\0')
+{
+len++;
+}
+for (index = 0; index < len; index++)
+{
+_putchar(str[index]);
+}
+_putchar('\n');
+}
