de1398027426da8a277475600b6bc18b1da7acf1
obasootom <onyearugbulem7@gmail.com>
2023-07-24 12:33:26 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - print the numbers since 0 - 9.
+ * @n: pointer to number
+ * Return: return the numbers 0 up to 9.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+
